12f9a36023cc91e914e1cbf5df7de254973ef842
Filip Sedivy <mail@filipsedivy.cz>
2016-11-15 10:55:22 +0100

    Aktualizace testů

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -6,17 +6,5 @@ use FilipSedivy\EET\Receipt;
 
 class ReceiptTest extends \PHPUnit_Framework_TestCase
 {
-    public function testReceiptObjectAttributes(){
-        $attributes = array(
-            'uuid_zpravy',
-            'dic_popl',
-            'id_provoz',
-            'id_pokl',
-            'celk_trzba'
-        );
 
-        foreach($attributes as $attribute){
-            $this->assertClassHasAttribute($attribute, new Receipt());
-        }
-    }
 }
