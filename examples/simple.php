fdea336e259f6864c2d35e1d04bfb4587a3eccf3
Filip Sedivy <mail@filipsedivy.cz>
2016-11-17 22:51:15 +0100

    Aktualizace příkladu

--- examples/simple.php
+++ examples/simple.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../../../../vendor/autoload.php';
 
 define('Playground', __DIR__.'/../src/Schema/PlaygroundService.wsdl');
 define('Production', __DIR__.'/../src/Schema/ProductionService.wsdl');
