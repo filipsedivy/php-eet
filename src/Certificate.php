dcccafcf0c5e8e43d155e39e6a7a130abf0b6190
Filip Sedivy <mail@filipsedivy.cz>
2016-11-17 22:41:24 +0100

    Vytvoření rozhraní pro práci s PKCS#12 rozhraní

---
+++ src/Certificate.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace FilipSedivy\EET;
+
+use FilipSedivy\EET\Exceptions\ClientException;
+
+/**
+ * Parsování PKCS#12 a uchování X.509 certifikátu
+ *
+ * @author Filip Šedivý <mail@filipsedivy.cz>
+ * @version 1.0.1
+*/
+class Certificate
+{
+    private $pkey;
+
+    private $cert;
+
+    public function __construct($certificate, $password)
+    {
+        if(!file_exists($certificate)){
+            throw new ClientException("Certifikat nebyl nalezen");
+        }
+
+        $certs = [];
+        $pkcs12 = file_get_contents($certificate);
+
+        $openSSL = openssl_pkcs12_read($pkcs12, $certs, $password);
+        if(!$openSSL)
+        {
+            throw new ClientException("Certifikat se nepodarilo vyexportovat.");
+        }
+
+        $this->pkey = $certs['pkey'];
+        $this->cert = $certs['cert'];
+    }
+
+    public function getPrivateKey(){
+        return $this->pkey;
+    }
+
+    public function getCert(){
+        return $this->cert;
+    }
+}
