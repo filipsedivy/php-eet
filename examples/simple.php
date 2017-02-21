985dbb1596772a9551cfa0d2be717513a41681d8
Filip Šedivý <mail@filipsedivy.cz>
2017-02-21 22:14:10 +0100

    Úprava příkladu

--- examples/simple.php
+++ examples/simple.php
@@ -1,9 +1,4 @@
 <?php
-
-require_once __DIR__.'/../../../../vendor/autoload.php';
-
-define('Playground', __DIR__.'/../src/Schema/PlaygroundService.wsdl');
-define('Production', __DIR__.'/../src/Schema/ProductionService.wsdl');
 /**
  * This file is part of the PHP-EET package.
  *
@@ -16,15 +11,16 @@ define('Production', __DIR__.'/../src/Schema/ProductionService.wsdl');
  * @author Filip Sedivy <mail@filipsedivy.cz>
  */
 
+require_once __DIR__.'/../vendor/autoload.php';
+
 use FilipSedivy\EET\Dispatcher;
 use FilipSedivy\EET\Receipt;
 use FilipSedivy\EET\Utils\UUID;
 use FilipSedivy\EET\Certificate;
 
 $certificate = new Certificate(__DIR__.'/EET_CA1_Playground-CZ00000019.p12', 'eet');
-$dispatcher = new Dispatcher(Playground, $certificate);
-
-$dispatcher->trace = true;
+$dispatcher = new Dispatcher($certificate);
+$dispatcher->setPlaygroundService();
 
 $uuid = UUID::v4();
 
@@ -39,12 +35,23 @@ $r->celk_trzba = 500;
 
 echo '<h2>---REQUEST---</h2>';
 echo "<pre>";
+
 try {
-    $fik = $dispatcher->send($r);
-    echo sprintf('<b>Returned FIK code: %s</b><br />', $fik);
-} catch (\FilipSedivy\EET\Exceptions\ServerException $e) {
-    var_dump($e); // See exception
-} catch (\Exception $e) {
-    var_dump($e); // Fatal error
+
+    $dispatcher->send($r);
+
+    // Tržba byla úspěšně odeslána
+    echo sprintf("FIK: %s <br>", $dispatcher->getFik());
+    echo sprintf("BKP: %s <br>", $dispatcher->getBkp());
+
+}catch(\FilipSedivy\EET\Exceptions\EetException $ex){
+    // Tržba nebyla odeslána
+
+    echo sprintf("BKP: %s <br>", $dispatcher->getBkp());
+    echo sprintf("PKP: %s <br>", $dispatcher->getPkp());
+
+}catch(Exception $ex){
+    // Obecná chyba
+    var_dump($ex);
+
 }
-echo sprintf('Request size: %d bytes | Response size: %d bytes | Response time: %f ms | Connection time: %f ms<br />', $dispatcher->getLastRequestSize(), $dispatcher->getLastResponseSize(), $dispatcher->getLastResponseTime(), $dispatcher->getConnectionTime()); // Size of transferred data
