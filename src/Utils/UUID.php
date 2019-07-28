9d4ba8818d48455de74bf44c256c35e534670098
Filip Šedivý <mail@filipsedivy.cz>
2019-07-28 21:08:51 +0200

    Set UUID deprecated

--- src/Utils/UUID.php
+++ src/Utils/UUID.php
@@ -6,8 +6,14 @@ use Ramsey;
 
 class UUID
 {
+    /**
+     * @return string
+     * @throws \Exception
+     * @deprecated Use Ramsey\Uuid\Uuid::uuid4() instead
+     */
     public static function v4(): string
     {
+        trigger_error(__METHOD__ . '() is deprecated; use Ramsey\Uuid\Uuid::uuid4() instead.', E_USER_DEPRECATED);
         return Ramsey\Uuid\Uuid::uuid4()->toString();
     }
 }
