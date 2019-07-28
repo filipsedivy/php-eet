166d6fb8edc30074791df69f870c735c6eb5797b
Filip Šedivý <mail@filipsedivy.cz>
2019-07-28 21:11:04 +0200

    declare(strict_types=1)

--- tests/EetTests/UtilsTest.phpt
+++ tests/EetTests/UtilsTest.phpt
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * Test: FilipSedivy\EET\Utils.
  *
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,4 @@
-<?php
-/**
- * This file is part of the PHP-EET package.
- *
- * (c) Filip Sedivy <mail@filipsedivy.cz>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @license MIT
- * @author Filip Sedivy <mail@filipsedivy.cz>
- */
+<?php declare(strict_types=1);
 
 use Tester\Environment;
 use Tester\Helpers;
