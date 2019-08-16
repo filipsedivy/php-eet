9cfc602ab04a21eb7aa024a9d74c4968bededaf1
Filip Šedivý <mail@filipsedivy.cz>
2019-08-17 01:27:23 +0200

    :pencil2: Implement PHP 7.x and refactoring by PHPCS

--- src/FilipSedivy/EET/Utils/Format.php
+++ src/FilipSedivy/EET/Utils/Format.php
@@ -4,15 +4,18 @@ namespace FilipSedivy\EET\Utils;
 
 class Format
 {
-    public static function BKB($code): string
+    public static function BKB(string $code): string
     {
         $r = '';
+
         for ($i = 0; $i < 40; $i++) {
             if ($i % 8 === 0 && $i !== 0) {
                 $r .= '-';
             }
+
             $r .= $code[$i];
         }
+
         return $r;
     }
 }
