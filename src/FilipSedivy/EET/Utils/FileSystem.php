08af42e82307862cb42de50b629633fdba1b428c
Filip Šedivý <mail@filipsedivy.cz>
2019-08-17 04:25:16 +0200

    🏠 Create simple file system class

--- src/FilipSedivy/EET/Utils/FileSystem.php
+++ src/FilipSedivy/EET/Utils/FileSystem.php
@@ -1,16 +1,34 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace FilipSedivy\EET\Utils;
 
+use FilipSedivy\EET\Exceptions;
+
 class FileSystem
 {
-    public static function isFileExists($path): bool
+    public static function isFileExists(string $file): bool
     {
-        return self::isFile($path) && file_exists($path);
+        return self::isFile($file) && file_exists($file);
     }
 
-    public static function isFile(string $path): bool
+    public static function isFile(string $file): bool
     {
-        return is_file($path);
+        return is_file($file);
+    }
+
+    public static function isReadable(string $file): bool
+    {
+        return self::isFile($file) && is_readable($file);
+    }
+
+    public static function read(string $file): string
+    {
+        $content = @file_get_contents($file);
+
+        if ($content === false) {
+            throw new Exceptions\IOException("Unable to read file '$file'.");
+        }
+
+        return $content;
     }
 }
