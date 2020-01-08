5d9578719ea8495cee03ca43660a5798b057fbc1
Filip Šedivý <mail@filipsedivy.cz>
2020-01-09 00:12:44 +0100

    Create new assert

--- tests/EETTests/ReceiptTest.php
+++ tests/EETTests/ReceiptTest.php
@@ -11,6 +11,7 @@ use FilipSedivy\EET\Certificate;
 use FilipSedivy\EET\Dispatcher;
 use FilipSedivy\EET\Exceptions\Receipt\ConstraintViolationException;
 use FilipSedivy\EET\Receipt;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
 use Tester\Assert;
 use Tester\TestCase;
 
@@ -41,6 +42,23 @@ class ReceiptTest extends TestCase
             $dispatcher->send($receipt);
         }, ConstraintViolationException::class);
     }
+
+    public function testConstraintViolation(): void
+    {
+        $certificate = new Certificate(__DIR__ . '/../Data/EET_CA1_Playground-CZ00000019.p12', 'eet');
+        $dispatcher = new Dispatcher($certificate, Dispatcher::PLAYGROUND_SERVICE, true);
+
+        $receipt = new Receipt;
+        $receipt->dic_popl = 'BadValue';
+
+        try {
+            $dispatcher->getCheckCodes($receipt);
+        } catch (ConstraintViolationException $exception) {
+            Assert::type('array', $exception->getErrors());
+            Assert::type('array', $exception->getProperties());
+            Assert::type(ConstraintViolationListInterface::class, $exception->getConstraintViolationList());
+        }
+    }
 }
 
-(new ReceiptTest())->run();
+(new ReceiptTest)->run();
