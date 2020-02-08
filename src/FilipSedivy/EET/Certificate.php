c965a2bda252d91f2d21fd76006e4a49928d5285
Filip Šedivý <mail@filipsedivy.cz>
2020-02-08 14:47:44 +0100

    New methods for working with certificate and possibilities of certificate validation

--- src/FilipSedivy/EET/Certificate.php
+++ src/FilipSedivy/EET/Certificate.php
@@ -2,6 +2,7 @@
 
 namespace FilipSedivy\EET;
 
+use DateTime;
 use FilipSedivy\EET\Exceptions;
 use FilipSedivy\EET\Utils;
 
@@ -12,6 +13,9 @@ class Certificate
 
     /** @var string */
     private $certificate;
+
+    /** @var array */
+    private $export;
 
     public function __construct(string $file, string $password)
     {
@@ -33,6 +37,7 @@ class Certificate
 
         $this->privateKey = $certs['pkey'];
         $this->certificate = $certs['cert'];
+        $this->export = openssl_x509_parse($this->getCertificate());
     }
 
     public function getPrivateKey(): string
@@ -43,5 +48,76 @@ class Certificate
     public function getCertificate(): string
     {
         return $this->certificate;
+    }
+
+    public function getExport(): array
+    {
+        return $this->export;
+    }
+
+    public function getIssuer(): array
+    {
+        $export = $this->getExport();
+
+        return $export['issuer'];
+    }
+
+    public function getSubject(): array
+    {
+        $export = $this->getExport();
+
+        return $export['subject'];
+    }
+
+    public function getValidFrom(): DateTime
+    {
+        $export = $this->getExport();
+
+        return date_create_from_format('ymdHise', $export['validFrom']);
+    }
+
+    public function getValidTo(): DateTime
+    {
+        $export = $this->getExport();
+
+        return date_create_from_format('ymdHise', $export['validTo']);
+    }
+
+    public function isIssuerOk(): bool
+    {
+        $production = [
+            'DC' => 'CZ',
+            'O' => 'Česká Republika – Generální finanční ředitelství',
+            'CN' => 'EET CA 1'
+        ];
+
+        $playground = [
+            'DC' => 'CZ',
+            'O' => 'Česká Republika – Generální finanční ředitelství',
+            'CN' => 'EET CA 1 Playground'
+        ];
+
+        return count(array_diff_assoc($this->getIssuer(), $production, $playground)) === 0;
+    }
+
+    public function isCertificateOk(): bool
+    {
+        $export = $this->getExport();
+
+        return $export['signatureTypeSN'] === 'RSA-SHA256';
+    }
+
+    public function isValidOk(): bool
+    {
+        $now = new DateTime('now');
+
+        return ($this->getValidTo() > $now) && ($this->getValidFrom() < $now);
+    }
+
+    public function isOk(): bool
+    {
+        return $this->isIssuerOk() &&
+            $this->isCertificateOk() &&
+            $this->isValidOk();
     }
 }
