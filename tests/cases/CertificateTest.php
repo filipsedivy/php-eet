e17f614fc6c5b93d634468ba4dcb9890e65b53bb
Filip Šedivý <mail@filipsedivy.cz>
2020-02-08 17:34:28 +0100

    Create new tests

--- tests/cases/CertificateTest.php
+++ tests/cases/CertificateTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Cases;
 
+use DateTime;
 use FilipSedivy\EET\Certificate;
 use FilipSedivy\EET\Exceptions;
 use Tester\Assert;
@@ -49,6 +50,39 @@ class CertificateTest extends TestCase
         } catch (Exceptions\Certificate\CertificateExportFailedException $exception) {
             Assert::type('string', $exception->getPath());
         }
+    }
+
+    public function testCertificateValidation(): void
+    {
+        $certificate = new Certificate(DATA_DIR . '/EET_CA1_Playground-CZ00000019.p12', 'eet');
+        $certificate2 = new Certificate(DATA_DIR . '/CA_TEST-01.p12', 'test');
+
+        Assert::true($certificate->isIssuerOk());
+        Assert::true($certificate->isValidOk());
+        Assert::true($certificate->isCertificateOk());
+        Assert::true($certificate->isOk());
+
+        Assert::false($certificate2->isIssuerOk());
+        Assert::true($certificate2->isValidOk());
+        Assert::true($certificate2->isCertificateOk());
+        Assert::false($certificate2->isOk());
+    }
+
+    public function testExport(): void
+    {
+        $certificate = new Certificate(DATA_DIR . '/EET_CA1_Playground-CZ00000019.p12', 'eet');
+
+        Assert::type('array', $certificate->getExport());
+        Assert::type('array', $certificate->getIssuer());
+        Assert::type('array', $certificate->getSubject());
+
+        Assert::type('bool', $certificate->isOk());
+        Assert::type('bool', $certificate->isValidOk());
+        Assert::type('bool', $certificate->isIssuerOk());
+        Assert::type('bool', $certificate->isCertificateOk());
+
+        Assert::type(DateTime::class, $certificate->getValidFrom());
+        Assert::type(DateTime::class, $certificate->getValidTo());
     }
 }
 
